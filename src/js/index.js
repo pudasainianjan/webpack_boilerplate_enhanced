edf6d79cc13d733d743b8ec2deccf0884aa03ab5
pudasainianjan <pudasainianjan@gmail.com>
2020-08-20 15:42:27 +0545

    added import export examples

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
-import num from './test';
-const x= 23;
-console.log(`I imported ${num} from another module called test.js! variable x is ${x}`);
+import str from './models/Search';
+//3 ways to import:
+// import {add,multiply,ID} from './view/searchView';
+//**wee could also us different name in above like this:
+// import {add as a,multiply as m,ID} from './view/searchView';
+import * as searchView from './view/searchView';
+
+
+//console.log(`using imported functions! ${add(ID,2)} and ${multiply(3,5)}...${str}`);
+//console.log(`using imported functions! ${a(ID,2)} and ${m(3,5)}...${str}`);
+const ID = searchView.ID;
+console.log(`using imported functions! ${searchView.add(ID,2)} and ${searchView.multiply(3,5)}...${str}`);
---
+++ src/js/view/searchView.js
@@ -0,0 +1,5 @@
+export const add = (a,b) => a+b;
+
+export const multiply = (a,b) => a*b;
+
+export const ID = 23;
